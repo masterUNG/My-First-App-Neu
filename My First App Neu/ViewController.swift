e00f6abf1290f8a832f981a7b9dcbfad5daef462
masterUNG <phrombutr@gmail.com>
2016-06-22 11:54:55 +0700

    Print Number OK

--- My First App Neu/ViewController.swift
+++ My First App Neu/ViewController.swift
@@ -9,17 +9,65 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var numberLabel: UILabel!
+    
+    //Explicit
+    var intNubmer:Int = 1
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+    }   // Main Function
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }   // Second Function
+    
+    
+    
+    @IBAction func increaseButton(sender: AnyObject) {
+        
+        intNubmer += 1
+        showNumber(intNubmer)
+        
+    }   // increaseButton
+    
+    
+    
+    @IBAction func decreaseButton(sender: AnyObject) {
+        
+        intNubmer -= 1
+        showNumber(intNubmer)
+        
+    }   // decreaseButton
+    
+    
+    func showNumber(myNumber:Int) -> Void {
+        print("myNumber = \(myNumber)")
     }
+    
+    
 
 
-}
+}   // Main Class
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
